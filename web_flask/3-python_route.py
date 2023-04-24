bb69f044be9f0b8981c6a29a023a668286bb637f
igraziella <igraziella2@gmail.com>
2023-04-23 19:57:34 -0700

    completed task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,6 +27,7 @@ def c():
     return 'C {}'.format(text.replace('_', ' '))
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """ Displays Python on port 5000, followed by
