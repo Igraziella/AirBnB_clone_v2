119a2ba25008616ec7a48b0ac83c1fa1c94c05a1
igraziella <igraziella2@gmail.com>
2023-04-23 19:33:33 -0700

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" A script thats starts a Flask application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Displays Hello HBNB on port 5000 """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays HBNB on port 5000 """
+    return "HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c():
+    """ Displays C on port 5000, followed by
+        the value of the text variable
+    """
+    return 'c {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
