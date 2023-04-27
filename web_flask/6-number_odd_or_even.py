1b1e7184a4e04dceb907e548918b7e58e7d232c3
igraziella <igraziella2@gmail.com>
2023-04-27 07:58:56 -0700

    updated task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,7 +3,7 @@
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -49,7 +49,7 @@ def number_template(n):
     """ Displays a HTML page on port 5000, if:
         n is an integer
     """
-    return num_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
@@ -58,7 +58,7 @@ def number_odd_or_even(n):
         n is an integer
         n is an odd|even number
     """
-    return num('6-number_odd_or_even.html', n=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
